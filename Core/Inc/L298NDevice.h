f0822484e3d0c24b8d9094fdab43a2ca6486c5f3
BDKeyLF <shamrin08@list.ru>
2026-03-24 01:02:08 +0300

    Getting started with L298NDevice

---
+++ Core/Inc/L298NDevice.h
@@ -0,0 +1,22 @@
+/*
+ * L298NDevice.h
+ *
+ *  Created on: 24 мар. 2026 г.
+ *      Author: Арсений
+ */
+
+#ifndef INC_L298NDEVICE_H_
+#define INC_L298NDEVICE_H_
+
+
+
+
+
+#endif /* INC_L298NDEVICE_H_ */
+
+#include "tim.h"
+#include "gpio.h"
+
+typedef __uint8_t  uint8_t;
+typedef __uint16_t uint16_t;
+typedef __uint32_t uint32_t;
---
+++ Core/Src/L298NDevice.cpp
@@ -0,0 +1,28 @@
+/*
+ * L298NDevice.cpp
+ *
+ *  Created on: 24 мар. 2026 г.
+ *      Author: Арсений
+ */
+#include "L298NDevice.h"
+
+class L298NDevice{
+public:
+	L298NDevice(TIM_HandleTypeDef& htimx, uint16_t TIM_CHANNEL, GPIO_TypeDef& GPIOx, uint16_t IN3_GPIO_Pin, uint16_t IN4_GPIO_Pin) {
+		this->htimx = &htimx;
+		this->TIM_CHANNEL=TIM_CHANNEL;
+		this->GPIOx = &GPIOx;
+		this->IN3_GPIO_Pin=IN3_GPIO_Pin;
+		this->IN4_GPIO_Pin=IN4_GPIO_Pin;
+		HAL_TIM_PWM_Start(&htimx, TIM_CHANNEL);
+	}
+	void SetSpeed(uint16_t speed){
+		__HAL_TIM_SET_COMPARE(&htimx,TIM_CHANNEL, speed);
+	}
+protected:
+	TIM_HandleTypeDef* htimx;
+	uint16_t TIM_CHANNEL;
+	GPIO_TypeDef* GPIOx;
+	uint16_t IN3_GPIO_Pin;
+	uint16_t IN4_GPIO_Pin;
+};
