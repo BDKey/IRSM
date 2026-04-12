a3f1e805b973a696a0e6353d64e0a1ac88cc2ae2
BDKeyLF <shamrin08@list.ru>
2026-04-12 10:58:30 +0300

    Still working on Menu implementation

--- Core/Src/Menu.cpp
+++ Core/Src/Menu.cpp
@@ -12,29 +12,34 @@ class Menu{
 public:
 	std::list<std::string> MenuItems;
 	uint8_t Cursor;
-	Menu(std::list<std::string> MenuItems){
+	uint8_t Offset;
+	Menu(std::list<std::string> MenuItems, uint8_t Offset = 0){
 		this->MenuItems=MenuItems;
 		Cursor=0;
+		this->Offset=Offset;
 	}
 	std::list<std::string> GetItemsToDisplay(uint8_t amount){
 		std::list<std::string> ItemsToDisplay {};
-		if (amount < 1 || Cursor + amount >= MenuItems.size()) return ItemsToDisplay;
+		if (amount < 1 || Cursor + Offset + amount >= MenuItems.size()) return ItemsToDisplay;
 		auto iter = MenuItems.begin();
-		for (int i=0;i<Cursor;i++) iter++;
+		for (int i=0;i<Cursor+Offset;i++) iter++;
 		for (int i=0; i<amount;i++)
 		{
 			ItemsToDisplay.push_back(*iter);
 			iter++;
 		}
-		ItemsToDisplay.resize(amount, ""); // For the compatibility with the LCD display (menu may be LESS than amount)
+		ItemsToDisplay.resize(amount, ""); // For the compatibility with the LCD display (menu may be LESS than amount) # Alles fur alles
 		return ItemsToDisplay;
 	}
 	void MoveUp() {
-		if (Cursor < (MenuItems.size() - 1)) {
+		if (Cursor+Offset < (MenuItems.size() - 1)) {
 			Cursor++;
 		}
 	}
 	void MoveDown(){
-		if (Cursor > 0) Cursor--;
+		if (Cursor+Offset > 0) Cursor--;
+	}
+	std::string GetCurrentItem() {
+
 	}
 };
